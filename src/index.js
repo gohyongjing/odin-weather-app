2d3fe17ca2ba106623a3a1873bf709b9ffca7ebb
Yong Jing <gohyongjing@gmail.com>
2022-06-04 22:05:20 +0800

    Add more dev dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("hello");
+import "./style.css";
+
+const body = document.querySelector('body');
+body.innerText = "hello";
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
